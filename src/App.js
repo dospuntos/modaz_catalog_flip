e6d7c9ff1e509b448103fe0f738f5a8cd2d71017
johan@dospuntos.no <johan@dospuntos.no>
2021-04-19 13:07:22 -0400

    Commit to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HTMLFlipBook from "react-pageflip";
 
-function App() {
+const PageCover = React.forwardRef((props, ref) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page page-cover" ref={ref} data-density="hard">
+      <div className="page-content">
+        <h2>{props.children}</h2>
+      </div>
     </div>
   );
-}
+});
 
-export default App;
+const Page = React.forwardRef((props, ref) => {
+  return (
+    <div className="page" ref={ref}>
+      <div className="page-content">
+        <h2 className="page-header">Page header - {props.number}</h2>
+        <div className="page-image"></div>
+        <div className="page-text">{props.children}</div>
+        <div className="page-footer">{props.number + 1}</div>
+      </div>
+    </div>
+  );
+});
+
+export default class DemoBook extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 0,
+      totalPage: 0,
+    };
+  }
+
+  nextButtonClick = () => {
+    this.flipBook.getPageFlip().flipNext();
+  };
+
+  prevButtonClick = () => {
+    this.flipBook.getPageFlip().flipPrev();
+  };
+
+  onPage = (e) => {
+    this.setState({
+      page: e.data,
+    });
+  };
+
+  componentDidMount() {
+    this.setState({
+      totalPage: this.flipBook.getPageFlip().getPageCount(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <HTMLFlipBook
+          width={550}
+          height={733}
+          size="fixed"
+          minWidth={315}
+          maxWidth={1000}
+          minHeight={400}
+          maxHeight={1533}
+          maxShadowOpacity={1}
+          showCover={true}
+          mobileScrollSupport={true}
+          onFlip={this.onPage}
+          onChangeOrientation={this.onChangeOrientation}
+          onChangeState={this.onChangeState}
+          className="demo-book"
+          ref={(el) => (this.flipBook = el)}
+        >
+          <PageCover>BOOK TITLE</PageCover>
+          <Page number={1}>Lorem ipsum...</Page>
+          <Page number={2}>Lorem ipsum...</Page>
+          <Page number={3}>Lorem ipsum...</Page>
+          <Page number={4}>Lorem ipsum...</Page>
+          <Page number={5}>Lorem ipsum...</Page>
+          <Page number={6}>Lorem ipsum...</Page>
+          <Page number={7}>Lorem ipsum...</Page>
+
+          <PageCover>THE END</PageCover>
+        </HTMLFlipBook>
+
+        <div className="container">
+          <div>
+            <button type="button" onClick={this.prevButtonClick}>
+              Previous page
+            </button>
+            [<span>{this.state.page}</span> of
+            <span>{this.state.totalPage}</span>]
+            <button type="button" onClick={this.nextButtonClick}>
+              Next page
+            </button>
+          </div>
+          <div>
+            State: <i>{this.state.state}</i>, orientation:{" "}
+            <i>{this.state.orientation}</i>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
